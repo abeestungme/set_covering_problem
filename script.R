b590f142daff93843a7018ac74d6925c1505f0e8
abeestungme <119252496+abeestungme@users.noreply.github.com>
2023-06-19 11:35:58 +0200

    Update script.R

--- script.R
+++ script.R
@@ -6,7 +6,7 @@ n_cols <- sample(1:50, 1)
 n_elements <- sample(1:50, 1)
 
 matrix1 <- matrix(sample(c(0, 1), n_rows*n_cols, replace = TRUE), ncol = n_cols)
-matrix1
+#matrix1
 
 col_names <- c()
 for (i in LETTERS) {
@@ -24,35 +24,66 @@ for (i in LETTERS) {
 colnames(matrix1) <- col_names[1:n_cols]
 rownames(matrix1) <- 1:n_rows
 
+
+ilosc_kolumn<-dim(matrix1)[2]
+koszty <- c()
+for (i in c(1:ilosc_kolumn)) {
+  koszty <- append(koszty, sample(10:50,1))
+}
+
+matrix2 <- rbind(matrix1, koszty)
 dane <- matrix1
-dane<-as.data.frame(dane)
-View(dane)
+dane <- as.data.frame(dane)
+#View(dane)
 dane_temp<-dane
 
 wysokosc<-as.numeric(length(t(dane))/length(dane))
-                      
+
 ############################################
 numery_pokojow_z_kamera<-c()
 nazwy_kamer<-c()
+min_koszt<-c()
+koszty_suma<-c()
 
 while (length(numery_pokojow_z_kamera) != wysokosc) {
-  maks_kol_with_name<-which.max(colSums(dane_temp))
-  maks_kol_num<-as.numeric(which.max(colSums(dane_temp)))
+  koszt_na_kamere <- c()
+  for (i in c(1:length(dane)))
+  {
+    koszt_na_kamere <- append(koszt_na_kamere,as.numeric(matrix2[wysokosc+1,i]))
+  }
+  for (i in c(1:length(dane)))
+  {
+    if (sum(dane_temp[,i]>0)) {
+      koszt_na_kamere[i] <- koszt_na_kamere[i]/sum(dane_temp[,i])
+    }
+    else {
+      koszt_na_kamere[i] <- 9999999
+    }
+  }
+  min_koszt <- append(min_koszt, min(koszt_na_kamere))
+  min_koszt_index <- as.numeric(match(min(koszt_na_kamere),koszt_na_kamere))
+  koszty_suma <- append(koszty_suma,koszty[min_koszt_index])
+  maks_kol_with_name <- names(dane)[min_koszt_index]
   
   for (i in c(1:wysokosc)) {
     
-    if (dane_temp [i,maks_kol_num] == 1){
+    if (dane_temp [i,min_koszt_index] == 1){
       numery_pokojow_z_kamera<-c(numery_pokojow_z_kamera,rownames(dane_temp[i,]))
       
       dane_temp[i,]<-0
       
-      if(!(paste(names(maks_kol_with_name)) %in% nazwy_kamer)){
-        nazwy_kamer<-c(nazwy_kamer,names(maks_kol_with_name))
+      if(!(paste(maks_kol_with_name) %in% nazwy_kamer)){
+        nazwy_kamer<-c(nazwy_kamer,maks_kol_with_name)
       }
     }
   }
-  
 }
+
 
 nazwy_kamer
 numery_pokojow_z_kamera
+sum(koszty_suma)
+
+
+
+#solver
